d4e7e7c7d943f98e888735e477d3019c0def9c52
Chris-Coleongco <christien.coleongco@gmail.com>
2024-12-02 19:57:20 -0700

    added stuff for string analysis

--- app/gatekeeper/gatekeeper.go
+++ app/gatekeeper/gatekeeper.go
@@ -1,8 +1,56 @@
 package gatekeeper
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"unicode"
+)
 
-func Uses_Shell(opened_files []string) bool {
+// the following are helpers for the functions: Is_Encrypted
+
+func strings_analysis(path_to_exec string) {
+
+	// open file and read all printable strings. check if the executable uses crypto libs
+
+	file, err := os.Open(path_to_exec)
+
+	if err != nil {
+		log.Println("couldn't open path_to_exec", err)
+	}
+
+	reader := bufio.NewReader(file)
+
+	var strings_buf [][]byte
+
+	var current_string []byte
+
+	for {
+		char, err := reader.ReadByte()
+
+		if err != nil {
+			// If EOF reached, break the loop
+			if err.Error() != "EOF" {
+				fmt.Println("end of file!")
+			}
+			break
+		}
+
+		if unicode.IsPrint(rune(char)) {
+			// Accumulate printable characters
+			current_string = append(current_string, char)
+		} else {
+			//if it isnt printable then you need to push the current_string onto the strings_buf
+
+			strings_buf = append(strings_buf, current_string)
+		}
+	}
+
+}
+
+func Interacts_With_Shell(opened_files []string) bool {
 
 	for line := range opened_files {
 		if strings.Contains("ptmx", opened_files[line]) {
@@ -15,13 +63,13 @@ func Uses_Shell(opened_files []string) bool {
 }
 
 func Is_Encrypted(packet string) { // note this function takes in one packet, the for loop should not be in here as it will get really fuckin messy
-	// look through packet in the logs to see whether it encrypts the data
 
-	// if the packet shows client hellos and server hellos followed by application data, then it probably is encrypted
+	strings_analysis()
+
 }
 
 func Static_Analysis() {
-
+	// strings command parsing for encryption libraries used by sliver (i believe they used crypto/tls)
 }
 
 func analyze_packet_fmt() { // analyze the packets printed into the sniffy.log
@@ -36,4 +84,4 @@ func analyze_packet_fmt() { // analyze the packets printed into the sniffy.log
 
 }
 
-func striker()
+func striker() // function to tally a score on whether process should be killed
---
+++ research/testing_sniff/go.mod
@@ -0,0 +1,8 @@
+module testing
+
+go 1.23.3
+
+require (
+	github.com/gopacket/gopacket v1.3.1 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+)
