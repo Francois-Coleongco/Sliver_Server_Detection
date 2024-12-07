0a68efc83dfd399d67e951cf160321a1651852e9
Chris-Coleongco <christien.coleongco@gmail.com>
2024-12-07 13:54:32 -0700

    okay thats it

--- strace_test/main.go
+++ strace_test/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	// Define the command to run and strace it
-	cmd := exec.Command("strace", "-p", "142936")
+	cmd := exec.Command("strace", "-p", "142936") // note this will be the pid of the /dev/pts that the implant is using
 
 	// Set up the command to capture stdout and stderr
 	cmd.Stdout = os.Stdout
