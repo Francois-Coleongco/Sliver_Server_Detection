e9fbca080aefb9cf26e53fcb7f062698c7f03548
Chris-Coleongco <christien.coleongco@gmail.com>
2024-12-15 20:31:00 -0700

    added helper funcss

---
+++ app/helpers/child_process_hunter.go
@@ -0,0 +1,20 @@
+package helpers
+
+import (
+	"os/exec"
+	"fmt"
+	"strings"
+)
+
+func Get_Children(pid string) []string {
+
+	cmd := exec.Command("ps", "--ppid", pid)
+
+	output, err := cmd.CombinedOutput()
+
+	if err != nil {
+		fmt.Println("err occurred on getting children of", pid, err)
+	}
+
+	return strings.Split(string(output), "\n")
+}
